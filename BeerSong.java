6fc9688dcdcd3cf5f80176deb860b2ff7cab2b4f
br9577 <br9577@gmail.com>
2016-09-29 18:37:18 -0400

    Create BeerSong.java
    The beersong java file

---
+++ BeerSong.java
@@ -0,0 +1,32 @@
+package beersong;
+
+/**
+ *
+ * @author Brandon
+ */
+public class Beersong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Beersong b = new Beersong();
+       b.Ninety_Nine_Bottles_of_Beer();
+       
+    }
+    
+    public void Ninety_Nine_Bottles_of_Beer(){
+ for(int i=99; i>0; i--){
+     if(i>2){
+      System.out.println(i + " bottles of beer on the wall " + i + " bottles of beer. Take one down pass it around " + (i-1) + " bottles of beer on the wall.");
+     } else if (i==2){
+               System.out.println(i + " bottles of beer on the wall " + i + " bottles of beer. Take one down pass it around " + (i-1) + " bottle of beer on the wall.");
+     } else if (i==1){
+              System.out.println(i + " bottle of beer on the wall " + i + " bottle of beer. Take one down pass it around no more bottles of beer on the wall.");
+
+     }
+ }
+ 
+ 
+}
+}
